bc61de625af8b9658a8dc4284edebc143abfc502
ocawthorne <ocawthorne@gmail.com>
2021-02-08 07:23:36 +0000

    initial-state-lab

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
 // your Bomb code here!
+// your ImageSlider code here!
+import React from 'react'
+
+class Bomb extends React.Component {
+   constructor(props) {
+      super()
+      this.state = {
+         secondsLeft: props.initialCount
+      }
+   }
+
+   render() {
+      const message = this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`
+      return (<p>{message}</p>)
+   }
+}
+
+export default Bomb
